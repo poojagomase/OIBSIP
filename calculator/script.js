b070a6c6cc8f22c8ba20e3fd803c3f96f2acab16
Pooja Gomase <poojagomase26@gmail.com>
2026-03-13 01:04:17 +0530

    Refactor add function for clarity and consistency

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,6 @@
-function add(){
-let a = Number(document.getElementById("num1").value);
-let b = Number(document.getElementById("num2").value);
-
-document.getElementById("result").innerHTML = "Result: " + (a+b);
+// Function to add two numbers entered by user
+function add() {
+ let a = Number(document.getElementById("num1").value);
+ let b = Number(document.getElementById("num2").value);
+ document.getElementById("result").innerHTML = "Result: " + (a + b);
 }
